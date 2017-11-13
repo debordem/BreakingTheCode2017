4da6109e7c98280675834e1862af9a205c61d533
Mr De Borde <debordem@isdedu.de>
2017-11-13 09:53:53 +0100

    Create ConsonantVowel.py

---
+++ ConsonantVowel.py
@@ -0,0 +1,30 @@
+###############################################
+# ConsonantVowel.py
+# Vandenburg 2015 / De Borde 2017
+##############################################
+
+# INSTRUCTIONS
+
+##############################################################
+# Improve the VowelsConsonants program to show the vowels
+# consonants and other characters in a separate line instead
+# of including other characters with the consonants.
+##############################################################
+
+
+
+userInput = input("Enter your sentence: ")
+vowels="AEIOUaeiou"
+
+displayVowels=""
+displayConsonants=""
+
+for letter in userInput:
+    if letter in vowels:
+        displayVowels = displayVowels + letter
+    else:
+        displayConsonants = displayConsonants + letter
+
+
+print("Vowels: " + displayVowels)
+print("Consonants: " + displayConsonants)
